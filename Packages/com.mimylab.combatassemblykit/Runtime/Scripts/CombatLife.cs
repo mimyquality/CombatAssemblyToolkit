f3ae2d50cd3c7a1aa2d3ef2db0f5028f56aa7918
MimyQuality <mimyquality@gmail.com>
2024-02-19 22:23:13 +0900

    初期設定

---
+++ Packages/com.mimylab.combatassemblykit/Runtime/Scripts/CombatLife.cs
@@ -0,0 +1,198 @@
+﻿/*
+Copyright (c) 2024 Mimy Quality
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+namespace MimyLab.CombatAssemblyKit
+{
+    using UdonSharp;
+    using UnityEngine;
+    using VRC.SDKBase;
+    using VRC.Udon;
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class CombatLife : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private int _maxHP = 10000;
+        [SerializeField]
+        private int _maxMP = 10000;
+        [SerializeField]
+        private int _maxSP = 10000;
+
+        [UdonSynced, FieldChangeCallback(nameof(IsDead))]
+        private bool _isDead = false;
+        [UdonSynced, FieldChangeCallback(nameof(HP))]
+        private int _hp;
+        [UdonSynced, FieldChangeCallback(nameof(MP))]
+        private int _mp;
+        [UdonSynced, FieldChangeCallback(nameof(SP))]
+        private int _sp;
+        [UdonSynced]
+        private int _buff;
+        [UdonSynced]
+        private int _debuff;
+
+        public int MaxHP { get => _maxHP; }
+        public int MaxMP { get => _maxMP; }
+        public int MaxSP { get => _maxSP; }
+
+        public bool IsDead
+        {
+            get => _isDead;
+            set
+            {
+                Initialize();
+
+                if (value) { _hp = 0; }
+                _isDead = value;
+                RequestSerialization();
+            }
+        }
+        public int HP
+        {
+            get => _hp;
+            set
+            {
+                _hp = Mathf.Clamp(value, 0, _maxHP);
+                RequestSerialization();
+            }
+        }
+        public int MP
+        {
+            get => _mp;
+            set
+            {
+                _mp = Mathf.Clamp(value, 0, _maxMP);
+                RequestSerialization();
+            }
+        }
+        public int SP
+        {
+            get => _sp;
+            set
+            {
+                _sp = Mathf.Clamp(value, 0, _maxSP);
+                RequestSerialization();
+            }
+        }
+        public CombatUnitBuff Buff { get => (CombatUnitBuff)_buff; }
+        public CombatUnitDebuff Debuff { get => (CombatUnitDebuff)_debuff; }
+
+        protected bool _initialized = false;
+        protected virtual void Initialize()
+        {
+            if (_initialized) { return; }
+
+            _hp = _maxHP;
+            _mp = _maxMP;
+            _sp = _maxSP;
+
+            _initialized = true;
+        }
+        private void Start()
+        {
+            Initialize();
+        }
+
+        /******************************
+         リソース関係の処理
+         ******************************/
+        public bool IsFatal(int damage)
+        {
+            Initialize();
+
+            return HP + damage < 1;
+        }
+
+        public bool IsInsufficient(int cost)
+        {
+            Initialize();
+
+            return MP + cost < 0;
+        }
+
+        public bool IsCrack(int brisance)
+        {
+            Initialize();
+
+            return SP + brisance < 1;
+        }
+
+        public bool HasResource(int damage, int cost, int brisance)
+        {
+            return !(IsFatal(damage) || IsInsufficient(cost) || IsCrack(brisance));
+        }
+        public bool HasResource(Vector3Int amount)
+        {
+            return HasResource(amount.x, amount.y, amount.z);
+        }
+
+        public void Consume(int damage, int cost, int brisance)
+        {
+            Initialize();
+
+            HP += damage;
+            MP += cost;
+            SP += brisance;
+        }
+        public void Consume(Vector3Int amount)
+        {
+            Consume(amount.x, amount.y, amount.z);
+        }
+
+
+        /******************************
+         バフ関係の処理
+         ******************************/
+        public bool HasBuff(CombatUnitBuff effect)
+        {
+            return _buff == (_buff | (int)effect);
+        }
+
+        public void AddBuff(CombatUnitBuff effect)
+        {
+            _buff |= (int)effect;
+            RequestSerialization();
+        }
+
+        public void ClearBuff(CombatUnitBuff effect)
+        {
+            _buff &= ~(int)effect;
+            RequestSerialization();
+        }
+
+        public void ClearBuffAll()
+        {
+            _buff = 0;
+            RequestSerialization();
+        }
+
+        /******************************
+         デバフ関係の処理
+         ******************************/
+        public bool HasDebuff(CombatUnitDebuff effect)
+        {
+            return _debuff == (_debuff | (int)effect);
+        }
+
+        public void AddDebuff(CombatUnitDebuff effect)
+        {
+            _debuff |= (int)effect;
+            RequestSerialization();
+        }
+
+        public void ClearDebuff(CombatUnitDebuff effect)
+        {
+            _debuff &= ~(int)effect;
+            RequestSerialization();
+        }
+
+        public void ClearDebuffAll()
+        {
+            _debuff = 0;
+            RequestSerialization();
+        }
+    }
+}
---
+++ Packages/com.mimylab.combatassemblykit/Runtime/Scripts/CombatUnit.cs
@@ -0,0 +1,99 @@
+﻿/*
+Copyright (c) 2024 Mimy Quality
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+namespace MimyLab.CombatAssemblyKit
+{
+    using System;
+    using UdonSharp;
+    using UnityEngine;
+    using VRC.SDKBase;
+    using VRC.Udon;
+    using VRC.SDK3.Components;
+
+    [Flags]
+    public enum CombatElement
+    {
+        None = 0,
+        Fire = 1 << 0,
+        Earth = 1 << 1,
+        Water = 1 << 2,
+        Air = 1 << 3,
+    }
+    [Flags]
+    public enum CombatUnitBuff
+    {
+        None = 0,
+        Dummy = 1 << 0,
+    }
+    [Flags]
+    public enum CombatUnitDebuff
+    {
+        None = 0,
+        Dummy = 1 << 0,
+    }
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class CombatUnit : UdonSharpBehaviour
+    {
+        public CombatLife life;
+        [SerializeField]
+        protected CombatSkill[] holdSkills = new CombatSkill[0];
+
+        protected bool _initialized = false;
+        protected virtual void Initialize()
+        {
+            if (_initialized) { return; }
+
+            for (int i = 0; i < holdSkills.Length; i++)
+            {
+                if (holdSkills[i]) { holdSkills[i].holder = this; }
+            }
+
+            _initialized = true;
+        }
+        private void Start()
+        {
+            Initialize();
+        }
+
+        public virtual void Action(int index)
+        {
+            if (index >= holdSkills.Length) { return; }
+            if (!holdSkills[index]) { return; }
+
+            if (life.IsDead) { return; }
+            var cost = holdSkills[index].Cost;
+            if (!life.HasResource(cost)) { return; }
+
+            life.Consume(cost);
+            holdSkills[index].Action();
+        }
+
+        public virtual void OnSkillHit(CombatSkill hitSkill)
+        {
+            var damage = hitSkill.Power;
+            for (int i = 0; i < holdSkills.Length; i++)
+            {
+                if (holdSkills[i])
+                {
+                    damage = holdSkills[i].Reaction(damage);
+                }
+            }
+
+            // SPをシールド値として扱うならここで換算？
+
+            // 最終的に通るダメージで死亡判定
+            if (life.IsDead = life.IsFatal(damage.x))
+            {
+                DeadAction();
+                return;
+            }
+            life.Consume(damage);
+        }
+
+        public virtual void DeadAction() { }
+    }
+}
